48e91c8745047fd209ac217774cbf298fd4bff1c
John Salas <salas.john@hotmail.com>
2019-08-20 18:30:34 -0500

    2019-08-20

---
+++ MyFirstWebApiM3/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyFirstWebApiM3.Entities;
+
+namespace MyFirstWebApiM3.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ MyFirstWebApiM3/Controllers/AuthorsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApiM3.Context;
+using MyFirstWebApiM3.Entities;
+
+namespace MyFirstWebApiM3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Author>> Get()
+        {
+            return _context.Authors.ToList();
+        }
+    }
+}
